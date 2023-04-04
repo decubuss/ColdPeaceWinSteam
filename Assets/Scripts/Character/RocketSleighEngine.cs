56071693864cb1edee55779d97122885a9815dcb
Tajut <tajutaplok@gmail.com>
2023-04-04 23:25:21 +0300

    Engine Load Displayer Base
    Displayer now updates current Engine load onToggle events
    The speed change is not included
    The States interconnection and dependencies are not included
    Engine HP detoriation on load is not included

--- Assets/Scripts/Character/RocketSleighEngine.cs
+++ Assets/Scripts/Character/RocketSleighEngine.cs
@@ -2,14 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum EngineLoad
+{
+    NotRunning,
+    Optimal,
+    High,
+    VeryHigh,
+    Extreme
+}
 public class RocketSleighEngine : MonoBehaviour
 {
     [SerializeField]
     public int EngineHealth = 100;
     [SerializeField]
     public float MoveSpeed = 10f;
+    //[SerializeField]
+    //private EngineLoadDisplayer EngineLoadDisplayer;
 
-    public bool isOptimaLoadl = false;
+    public bool isNotRunning = true;
+    public bool isOptimaLoad = false;
     public bool isHighLoad = false;
     public bool isVeryHighLoad = false;
     public bool isExtremeLoad = false;
@@ -22,6 +33,28 @@ public class RocketSleighEngine : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (isNotRunning == true) { MoveSpeed = 0f; }
     }
+
+    public void OptimalHandler(bool isOn)
+    {
+        isOptimaLoad = isOn;
+        //Debug.Log("Optimal da value is " + OptimalToggle.isOn);
+    }
+    public void HighHandler(bool isOn)
+    {
+        isHighLoad = isOn;
+        //Debug.Log("High da value is " + HighToggle.isOn);
+    }
+    public void VeryHighHandler(bool isOn)
+    {
+        isVeryHighLoad = isOn;
+        //Debug.Log("Very High da value is " + VeryHighToggle.isOn);
+    }
+    public void ExtremeHandler(bool isOn)
+    {
+        isExtremeLoad = isOn;
+        //Debug.Log("Extreme da value is " + ExtremeToggle.isOn);
+    }
+
 }
--- Assets/Scripts/UI/EngineLoadDisplayer.cs
+++ Assets/Scripts/UI/EngineLoadDisplayer.cs
@@ -1,13 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EngineLoadDisplayer : MonoBehaviour
 {
+    [SerializeField]
+    private Toggle OptimalToggle;
+    [SerializeField]
+    private Toggle HighToggle;
+    [SerializeField]
+    private Toggle VeryHighToggle;
+    [SerializeField]
+    private Toggle ExtremeToggle;
+
+    [SerializeField]
+    private RocketSleighEngine SleighEngine;
+
+    public EngineLoad EngineLoad = EngineLoad.NotRunning;
     // Start is called before the first frame update
     void Start()
     {
-        
+        OptimalToggle.onValueChanged.AddListener(SleighEngine.OptimalHandler);
+        HighToggle.onValueChanged.AddListener(SleighEngine.HighHandler);
+        VeryHighToggle.onValueChanged.AddListener(SleighEngine.VeryHighHandler);
+        ExtremeToggle.onValueChanged.AddListener(SleighEngine.ExtremeHandler);
+
     }
 
     // Update is called once per frame
@@ -15,4 +33,5 @@ public class EngineLoadDisplayer : MonoBehaviour
     {
         
     }
+
 }
