f43c5fb09e23f30912c6c38dfc1a2bcf0afdf7cf
Tajut <tajutaplok@gmail.com>
2023-04-22 17:30:18 +0300

    Can choose Destination Now
    Can choose Destination Now
    But now canvas rules are changed - canvas overlay eferything, the route line is now disabled because it iis invisible
    next target is to do something with the end of the line
    
    or maybe road encounters

---
+++ Assets/Scripts/UI/InteractiveMap.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class InteractiveMap : MonoBehaviour, IPointerClickHandler
+{
+
+    // Start is called before the first frame update
+    [SerializeField]
+    private Image MapImage;
+    [SerializeField]
+    private Image DestinationPin;
+    [SerializeField]
+    private RectTransform localSpace;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerClick(PointerEventData eventData) // 3
+    {
+        //print("I was clicked");
+        //print(eventData.position);
+        Debug.Log(localSpace.InverseTransformPoint(eventData.position));
+        var desiredPosition = localSpace.InverseTransformPoint(eventData.position);
+        //DestinationPin.rectTransform.parent = null;
+        DestinationPin.rectTransform.localPosition = desiredPosition;
+        //TODO: fix localspace convertion it is nnot working at the moment
+        //DestinationPin.rectTransform.localPosition = MapImage.rectTransform.InverseTransformPoint((Vector3)eventData.position);
+    }
+}
