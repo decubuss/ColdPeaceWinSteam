bb579228563e71c6afbd14b0459811fb958c53cf
Andrei Astafev <xiaomao472@gmail.com>
2023-09-06 22:06:42 +0300

    Time Resource Alternative
    Prepartion to Reigns inspired Pivot - choose direction, choose encounter reactions.
    
    lead them, abandon or worse.
    
    Time flows while moving and on each decision

--- Assets/Scripts/UI/MapPanel/TimeResourceDisplay.cs
+++ Assets/Scripts/UI/MapPanel/TimeResourceDisplay.cs
@@ -1,18 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimeResourceDisplay : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private Text MinutesText;
+    [SerializeField]
+    private EnviromentInputs EnviromentInputs;
+
+
+    private float StormTimer = 0;
+    private void Awake()
     {
-        
+        StormTimer = EnviromentInputs.TimeBeforeStormGlobal;
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+
+    }
+    void FixedUpdate()
+    {
+        StormTimer -= 1 * Time.deltaTime;//TODO: make subtraction: on the move and after decesion made
+        MinutesText.text = MinutesDisplay(TimeSpan.FromSeconds((double)StormTimer));
+    }
+    private string MinutesDisplay(TimeSpan span)
+    {
+        return string.Format("{0}",
+                 (int)span.TotalMinutes);
     }
 }
