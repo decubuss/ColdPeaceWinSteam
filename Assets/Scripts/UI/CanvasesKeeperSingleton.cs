0927765ef7eb254dc2950bb069f70242690b850a
Andrei Astafev <xiaomao472@gmail.com>
2023-10-05 21:41:23 +0300

    Singleton to store Canvases
    Singleton to store Canvases - on our way to call encounter canvas, once player accepts that he wants to proceed with it

---
+++ Assets/Scripts/UI/CanvasesKeeperSingleton.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasesKeeperSingleton : MonoBehaviour
+{
+    public static GameObject MapCanvas;
+    public static GameObject EncounterCanvas;
+    public static Location CurrentLocation;
+    private void Awake()
+    {
+        MapCanvas = GameObject.Find("MapCanvas");
+        EncounterCanvas = GameObject.Find("EncounterCanvas");
+    }
+
+    private void Start()
+    {
+        if (MapCanvas != null) Debug.Log(MapCanvas);
+        if (EncounterCanvas != null)
+        {
+            EncounterCanvas.SetActive(false);
+            Debug.Log(EncounterCanvas);
+        }
+    }
+
+    public static void OpenMap()
+    {
+        EncounterCanvas.SetActive(false);
+        MapCanvas.SetActive(true);
+    }
+
+    public static void OpenEncounter(/*Location encounter*/)
+    {
+        MapCanvas.SetActive(false);
+        EncounterCanvas.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/UI/EncounterWindow/EncounterEnteringWindow.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class EncounterEnteringWindow : MonoBehaviour
+{
+    [SerializeField]
+    private Location _encounter;
+    [SerializeField]
+    public Location Encounter
+    {
+        get
+        {
+            return _encounter;
+        }
+        set
+        {
+            if (_encounter == null) 
+            {
+                _encounter = value;
+                LocationSet();
+            }
+            _encounter = value; 
+        }
+
+    }
+
+    [SerializeField]
+    private TextMeshProUGUI EncounterTitle;
+    [SerializeField]
+    private TextMeshProUGUI EncounterEnteringText;
+    [SerializeField]
+    private TextMeshProUGUI EncounterCost;
+
+    void Update()
+    {
+        HideIfClickedOutside();
+    }
+
+    private void HideIfClickedOutside()
+    {
+        var panel = gameObject;
+        if (Input.GetMouseButton(0) && panel.activeSelf &&
+            !RectTransformUtility.RectangleContainsScreenPoint(
+                panel.GetComponent<RectTransform>(),
+                Input.mousePosition,
+                Camera.main))
+        {
+            HideWindow();
+        }
+    }
+    private void LocationSet()
+    {
+        EncounterTitle.text = Encounter.Name;
+        EncounterEnteringText.text = CreateEnteringText(Encounter.Name);
+        EncounterCost.text = CreateEnteringCostText(Encounter.enteringCost);
+    }
+
+    private string CreateEnteringText(string encounterName)
+    {
+        string enteringText = "Stop by and search " + encounterName + "?";
+        return enteringText;
+    }
+
+    private string CreateEnteringCostText(int cost)
+    {
+        string costText = "Cost: " + cost.ToString() + " minutes";
+        return costText;
+    }
+
+    public void HideWindow()
+    {
+        gameObject.SetActive(false);
+    }
+    public void EnterEncounter()
+    {
+        CanvasesKeeperSingleton.OpenEncounter();
+    }
+}
