0c9323c2b695356eef674d360b0dabf4c738e842
Andrei Astafev <xiaomao472@gmail.com>
2023-10-09 21:59:23 +0300

    Variables are connected - Encouter Window
    Variables are connected - Encouter Window
    Now we need to update them on not null

---
+++ Assets/EncounterPanel.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class EncounterPanel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    public Location CurrentEncounter;
+
+    [Tooltip("Encounter general ")]
+    [SerializeField]
+    private TextMeshProUGUI EncounterTitle;
+
+    [Tooltip("Actual person or object of interaction")]
+    [SerializeField]
+    private TextMeshProUGUI EncounterSubject;
+
+    [Tooltip("Dialogue line of subject")]
+    [SerializeField]
+    private TextMeshProUGUI EncounterLine;
+
+    private void OnEnable()
+    {
+        CurrentEncounter = CanvasesKeeperSingleton.CurrentLocation;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CurrentEncounter != null)
+        {
+
+        }
+    }
+}
