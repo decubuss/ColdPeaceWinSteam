8b79b437ad6d9b4062e0ab4c14637323102d5102
Andrei Astafev <xiaomao472@gmail.com>
2023-10-12 21:55:32 +0300

    AFCell stats added
    AFCell stats added
    But not parsing - looks like we are not doing the parsing stuff for it

--- Assets/Scripts/LocationLogic/ArmedForcesLocation.cs
+++ Assets/Scripts/LocationLogic/ArmedForcesLocation.cs
@@ -4,6 +4,63 @@ using UnityEngine;
 
 public class ArmedForcesLocation : Location
 {
+    
+    [Tooltip("will to continue fight, the logic behind continuing doing the job")]
+    [SerializeField]
+    private float _battleMorale;
+    [Tooltip("thoughts and reasoning behind selfpreservation, undearstanding that you are on your own")]
+    [SerializeField]
+    private float _selfPreservation;
+    [Tooltip("level of acceptance of non-conventional decisions, reverse level of hostility")]
+    [SerializeField]
+    private float _mentalAgility;
+    [Tooltip("level of acceptance of AF organization and duties")]
+    [SerializeField]
+    private float _discipline;
+    public float BattleMorale
+    {
+        get
+        {
+            return _battleMorale;
+        }
+        set
+        {
+            _battleMorale = value;
+        }
+    }
+    public float SelfPreservation
+    {
+        get
+        {
+            return _selfPreservation;
+        }
+        set
+        {
+            _selfPreservation = value;
+        }
+    }
+    public float MentalAgility
+    {
+        get
+        {
+            return _mentalAgility;
+        }
+        set
+        {
+            _mentalAgility = value;
+        }
+    }
+    public float Discipline
+    {
+        get
+        {
+            return _discipline;
+        }
+        set
+        {
+            _discipline = value;
+        }
+    }
     public void Arrival()
     {
         
